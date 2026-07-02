d668d628bc73365810fa88ccc9aa8209e1008126
Kiran Tajanpure <129212680+kiran28092003@users.noreply.github.com>
2026-07-02 10:40:09 +0000

    some changes done

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,1 +1,15 @@
-console.log("Kiran")
+const accountId = 14456
+let accountEmail = "kiran123@gmail.com"
+var accountPass = "123"
+accountCity = "Mumbai"
+let accountState;
+// accountId = 2   //not allowed because const
+console.log(accountId);
+accountEmail = "Viran123@gmail.com"
+console.table([accountId, accountEmail, accountPass, accountCity,accountState]);
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+ */
+
+
---
+++ .devcontainer/01_basics/test2.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+//alert( 3 + 3 ) //we arer using node js not browser
+
+//documentation => 1.mdn 2.tc39.ts
+
+let name = "kiran"
+let age = 23
+let isLoggedIn = false
+//Datatype
+//number => 2 to power 53
+//bigInt 
+//String
+//boolean
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+console.log(typeof "kiran");
+console.log(typeof undefined);
+console.log(null);
+
